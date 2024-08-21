cd50a16679ad364585046b345a2393eab6aa902d
Andrey Zuev <stepgt@mail.ru>
2024-08-21 14:54:18 +1000

    Added CheckboxFiltersGroup component

--- src/components/shared/filters.tsx
+++ src/components/shared/filters.tsx
@@ -3,6 +3,7 @@ import { Title } from './title';
 import { FilterCheckbox } from './filter-checkbox';
 import { Input } from '../ui';
 import { RangeSlider } from './range-slider';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
 
 interface Props {
   className?: string;
@@ -27,6 +28,28 @@ export const Filters: React.FC<Props> = ({ className }) => {
 
         <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
       </div>
+
+      <CheckboxFiltersGroup
+        title="Ингредиенты"
+        className="mt-5"
+        limit={6}
+        defaultItems={[
+          { text: 'Сырный соус', value: '1' },
+          { text: 'Моццарелла', value: '2' },
+          { text: 'Чеснок', value: '3' },
+          { text: 'Солёные огурчики', value: '4' },
+          { text: 'Красный лук', value: '5' },
+          { text: 'Томаты', value: '6' },
+        ]}
+        items={[
+          { text: 'Сырный соус', value: '1' },
+          { text: 'Моццарелла', value: '2' },
+          { text: 'Чеснок', value: '3' },
+          { text: 'Солёные огурчики', value: '4' },
+          { text: 'Красный лук', value: '5' },
+          { text: 'Томаты', value: '6' },
+        ]}
+      />
     </div>
   );
 };
