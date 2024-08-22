637c4c06e7cd58c96e81e714338780d556fad900
Andrey Zuev <stepgt@mail.ru>
2024-08-22 16:31:57 +1000

    Added ProductsGroupList component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -7,3 +7,4 @@ export  { Filters } from './filters';
 export { FilterCheckbox } from './filter-checkbox';
 export { RangeSlider } from './range-slider';
 export { ProductCard } from './product-card';
+export { ProductsGroupList } from './products-group-list';
