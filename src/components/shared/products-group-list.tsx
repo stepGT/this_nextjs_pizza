0184a8399bd2d876bdb520c6aadf00f73f85fdfc
Andrey Zuev <stepgt@mail.ru>
2024-08-23 15:52:10 +1000

    Added react-use & useIntersection hook

--- src/components/shared/products-group-list.tsx
+++ src/components/shared/products-group-list.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import { useIntersection } from 'react-use';
 import { Title } from './title';
 import { cn } from '@/lib/utils';
 import { ProductCard } from './product-card';
@@ -20,8 +21,17 @@ export const ProductsGroupList: React.FC<Props> = ({
   categoryID,
   className,
 }) => {
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      console.log(title, categoryID)
+    }
+  }, [categoryID, intersection?.isIntersecting, title]);
   return (
-    <div className={className} id={title}>
+    <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
 
       <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
