827e8b2a96b1e419766588c37d11157e1d97bd2d
Andrey Zuev <stepgt@mail.ru>
2024-10-02 16:16:32 +1000

    Added SearchInput component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -8,3 +8,4 @@ export { FilterCheckbox } from './filter-checkbox';
 export { RangeSlider } from './range-slider';
 export { ProductCard } from './product-card';
 export { ProductsGroupList } from './products-group-list';
+export { SearchInput } from './search-input';
