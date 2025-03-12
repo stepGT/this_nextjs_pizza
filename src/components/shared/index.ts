cde64a808b139a9fae97377bf450b787b7340e6c
Andrey Zuev <stepgt@mail.ru>
2025-03-12 11:53:23 +1000

    Added ChooseProductModal to index

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -11,3 +11,4 @@ export { ProductCard } from './product-card';
 export { ProductsGroupList } from './products-group-list';
 export { SearchInput } from './search-input';
 export { GroupVariants } from './group-variants';
+export * from './modals';
