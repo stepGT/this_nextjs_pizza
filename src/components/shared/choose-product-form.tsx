83ff3b6af544052efbcd60e2349b026a71478d54
Andrey Zuev <stepgt@mail.ru>
2025-03-13 16:20:34 +1000

    Changed ProductImage to img

--- src/components/shared/choose-product-form.tsx
+++ src/components/shared/choose-product-form.tsx
@@ -29,7 +29,13 @@ export const ChooseProductForm: React.FC<Props> = ({
   const totalPrice = 1000;
   return (
     <div className={cn(className, 'flex flex-1')}>
-      <ProductImage imageUrl={imageUrl} size={30} />
+      <div className={cn('flex items-center justify-center flex-1 relative w-full', className)}>
+        <img
+          src={imageUrl}
+          alt={name}
+          className="relative left-2 top-2 transition-all z-10 duration-300 w-[350px] h-[350px]"
+        />
+      </div>
 
       <div className="w-[490px] bg-[#f7f6f5] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
