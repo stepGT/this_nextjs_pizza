189bf24cba06621b06c970c99afb9a50f9d08fc1
Andrey Zuev <stepgt@mail.ru>
2025-04-04 16:24:16 +1000

    Added handleClickAdd

--- src/components/shared/choose-pizza-form.tsx
+++ src/components/shared/choose-pizza-form.tsx
@@ -4,7 +4,7 @@ import { PizzaImage } from './pizza-image';
 import { Title } from './title';
 import { Button } from '../ui';
 import { GroupVariants } from './group-variants';
-import { PizzaSize, pizzaSizes, PizzaType, pizzaTypes } from '@/constants/pizza';
+import { mapPizzaType, PizzaSize, pizzaSizes, PizzaType, pizzaTypes } from '@/constants/pizza';
 import { Ingredient, ProductItem } from '@prisma/client';
 import { IngredientItem } from './ingredient-item';
 import { useSet } from 'react-use';
@@ -14,7 +14,7 @@ interface Props {
   name: string;
   ingredients: Ingredient[];
   items: ProductItem[];
-  onClickAddCard: (itemID: number, ingredients: number[]) => void;
+  onClickAddCard?: VoidFunction;
   className?: string;
 }
 
@@ -32,7 +32,24 @@ export const ChoosePizzaForm: FC<Props> = ({
   const [size, setSize] = useState<PizzaSize>(20);
   const [type, setType] = useState<PizzaType>(1);
   const [selectedIngredients, { toggle: addIngredient }] = useSet(new Set<number>([]));
-  const pizzaPrice = items.find(i => i.pizzaType === type && i.size === size)?.price;
+  const pizzaPrice = items.find((i) => i.pizzaType === type && i.size === size)?.price || 0;
+  const totalIngredientsPrice = ingredients
+    .filter((v) => selectedIngredients.has(v.id))
+    .reduce((acc, ingredient) => acc + ingredient.price, 0);
+  const totalPrice = pizzaPrice + totalIngredientsPrice;
+  const textDetails = `${size}см, ${mapPizzaType[type]} пицца`;
+
+  const handleClickAdd = () => {
+    onClickAddCard?.();
+  };
+
+  const availablePizzas = items.filter((v) => v.pizzaType === type);
+  const availablePizzaSizes = pizzaSizes.map((item) => ({
+    name: item.name,
+    value: item.value,
+    disabled: !availablePizzas.some((pizza) => Number(pizza.size) === Number(item.value)),
+  }));
+
   return (
     <div className={cn(className, 'flex flex-1')}>
       <PizzaImage imageUrl={imageUrl} size={size} />
@@ -40,11 +57,11 @@ export const ChoosePizzaForm: FC<Props> = ({
       <div className="w-[490px] bg-[#f7f6f5] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
 
-        <p className="text-gray-400">30см, традиционное тесто 30</p>
+        <p className="text-gray-400">{textDetails}</p>
 
         <div className="flex flex-col gap-4 mt-5">
           <GroupVariants
-            items={pizzaSizes}
+            items={availablePizzaSizes}
             value={String(size)}
             onClick={(value) => setSize(Number(value) as PizzaSize)}
           />
@@ -72,9 +89,9 @@ export const ChoosePizzaForm: FC<Props> = ({
         </div>
 
         <Button
-          onSubmit={() => {}}
+          onClick={handleClickAdd}
           className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
-          Добавить в корзину за {pizzaPrice} ₽
+          Добавить в корзину за {totalPrice} ₽
         </Button>
       </div>
     </div>
