7037c2d3dc73c76e4e10637eb4af95adc09335e7
Andrey Zuev <stepgt@mail.ru>
2025-04-25 14:09:22 +1000

    Added hook usePizzaOptions & Added lib getPizzaDetails

---
+++ src/hooks/usePizzaOptions.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { PizzaSize, PizzaType } from '@/constants/pizza';
+import { Variant } from '../components/shared/group-variants';
+import { useSet } from 'react-use';
+import { getAvailablePizzaSizes } from '../lib';
+import { ProductItem } from '@prisma/client';
+
+interface ReturnProps {
+  size: PizzaSize;
+  type: PizzaType;
+  selectedIngredients: Set<number>;
+  availableSizes: Variant[];
+  currentItemId?: number;
+  setSize: (size: PizzaSize) => void;
+  setType: (size: PizzaType) => void;
+  addIngredient: (id: number) => void;
+}
+
+export const usePizzaOptions = (items: ProductItem[]): ReturnProps => {
+  const [size, setSize] = useState<PizzaSize>(20);
+  const [type, setType] = useState<PizzaType>(1);
+  const [selectedIngredients, { toggle: addIngredient }] = useSet(new Set<number>([]));
+
+  const availableSizes = getAvailablePizzaSizes(type, items);
+
+  const currentItemId = items.find((item) => item.pizzaType === type && item.size === size)?.id;
+
+  useEffect(() => {
+    const isAvailableSize = availableSizes?.find(
+      (item) => Number(item.value) === size && !item.disabled,
+    );
+    const availableSize = availableSizes?.find((item) => !item.disabled);
+
+    if (!isAvailableSize && availableSize) {
+      setSize(Number(availableSize.value) as PizzaSize);
+    }
+  }, [type]);
+
+  return {
+    size,
+    type,
+    selectedIngredients,
+    availableSizes,
+    currentItemId,
+    setSize,
+    setType,
+    addIngredient,
+  };
+};
