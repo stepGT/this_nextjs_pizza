2cc957999689be193a8248818fee138140a5ceac
Andrey Zuev <stepgt@mail.ru>
2025-06-30 16:31:58 +1000

    Added lib findPizzas

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,19 +1,10 @@
 import { Suspense } from 'react';
 import { Container, Filters, ProductsGroupList, Title } from '@/components/shared';
 import { TopBar } from '@/components/shared/top-bar';
-import { prisma } from '../../../prisma/prisma-client';
+import { findPizzas, GetSearchParams } from '@/lib/find-pizzas';
 
-export default async function Home() {
-  const categories = await prisma.category.findMany({
-    include: {
-      products: {
-        include: {
-          ingredients: true,
-          items: true,
-        },
-      },
-    },
-  });
+export default async function Home({ searchParams }: { searchParams: GetSearchParams }) {
+  const categories = await findPizzas(searchParams);
 
   return (
     <>
