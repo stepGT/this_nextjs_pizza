3d56b14cea371aabbe413f1577bd50e572b01187
Andrey Zuev <stepgt@mail.ru>
2025-07-15 16:17:55 +1000

    Added CheckoutLayout & CheckoutPage

---
+++ src/app/(checkout)/checkout/page.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from '@/components/shared';
+
+export default function CheckoutPage() {
+  return (
+    <Container className="mt-10">
+      <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
+    </Container>
+  );
+}
