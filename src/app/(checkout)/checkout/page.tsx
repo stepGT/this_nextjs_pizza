fb3e885a4a50284903dfbc7d476e99d5958d1333
Andrey Zuev <stepgt@mail.ru>
2025-07-16 15:19:28 +1000

    Added component Textarea & Markuped checkout page

--- src/app/(checkout)/checkout/page.tsx
+++ src/app/(checkout)/checkout/page.tsx
@@ -1,9 +1,49 @@
-import { Container, Title } from '@/components/shared';
+import { Container, Title, WhiteBlock } from '@/components/shared';
+import { Input, Textarea } from '@/components/ui';
 
 export default function CheckoutPage() {
   return (
     <Container className="mt-10">
       <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
+      <div className="flex gap-10">
+        <div className="flex flex-col gap-10 flex-1 mb-20">
+          <WhiteBlock title="1. Cart">qwerty</WhiteBlock>
+
+          <WhiteBlock title="2. Personal data">
+            <div className="grid grid-cols-2 gap-5">
+              <Input name="firstName" className="text-base" placeholder="Name" />
+              <Input name="lastName" className="text-base" placeholder="Lastname" />
+              <Input name="email" className="text-base" placeholder="E-Mail" />
+              <Input name="phone" className="text-base" placeholder="Phone" />
+            </div>
+          </WhiteBlock>
+
+          <WhiteBlock title="3. Delivery address">
+            <div className="grid grid-col gap-5">
+              <Input name="address" className="text-base" placeholder="Address" />
+              <Textarea placeholder="Comment" className="text-base" rows={5} />
+            </div>
+          </WhiteBlock>
+        </div>
+
+        <div className="w-[450px]">
+          <WhiteBlock className="p-6 sticky top-4">
+            <div className="flex flex-col gap-1">
+              <span className="text-xl">Total</span>
+              <span className="text-[34px] font-extrabold">3506 ₽</span>
+            </div>
+
+            <div className="flex my-4">
+              <span className="flex flex-1 text-lg text-neutral-500">
+                Cost of product:
+                <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
+              </span>
+
+              <span className='font-bold text-lg'>3000 ₽</span>
+            </div>
+          </WhiteBlock>
+        </div>
+      </div>
     </Container>
   );
 }
