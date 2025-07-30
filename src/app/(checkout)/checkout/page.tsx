198f49ba8df3301261930fde20e481c9b0f6bfb9
Andrey Zuev <stepgt@mail.ru>
2025-07-30 16:34:19 +1000

    Added FormProvider to CheckoutPage & Refactor FormInput component

--- src/app/(checkout)/checkout/page.tsx
+++ src/app/(checkout)/checkout/page.tsx
@@ -6,39 +6,62 @@ import { Input, Textarea } from '@/components/ui';
 import { useCart } from '@/hooks';
 import { CheckoutCart } from '@/components/shared/checkout';
 import { CheckoutPersonalForm } from '@/components/shared/checkout/checkout-personal-form';
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { checkoutFormSchema, CheckoutFormValues } from '@/constants';
 
 export default function CheckoutPage() {
   const { totalAmount, updateItemQuantity, items, removeCartItem, loading } = useCart();
+  const form = useForm<CheckoutFormValues>({
+    resolver: zodResolver(checkoutFormSchema),
+    defaultValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      phone: '',
+      address: '',
+      comment: '',
+    },
+  });
   const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
     const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
     updateItemQuantity(id, newQuantity);
   };
+  const onSubmit = async (data: CheckoutFormValues) => {
+    console.log(data)
+    try {
+    } catch (err) {}
+  };
   return (
     <Container className="mt-10">
       <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
-      <div className="flex gap-10">
-        <div className="flex flex-col gap-10 flex-1 mb-20">
-          <CheckoutCart
-            onClickCountButton={onClickCountButton}
-            removeCartItem={removeCartItem}
-            items={items}
-            loading={loading}
-          />
+      <FormProvider {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex gap-10">
+            <div className="flex flex-col gap-10 flex-1 mb-20">
+              <CheckoutCart
+                onClickCountButton={onClickCountButton}
+                removeCartItem={removeCartItem}
+                items={items}
+                loading={loading}
+              />
 
-          <CheckoutPersonalForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
+              <CheckoutPersonalForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
 
-          <WhiteBlock title="3. Delivery address">
-            <div className="grid grid-col gap-5">
-              <Input name="address" className="text-base" placeholder="Address" />
-              <Textarea placeholder="Comment" className="text-base" rows={5} />
+              <WhiteBlock title="3. Delivery address">
+                <div className="grid grid-col gap-5">
+                  <Input name="address" className="text-base" placeholder="Address" />
+                  <Textarea placeholder="Comment" className="text-base" rows={5} />
+                </div>
+              </WhiteBlock>
             </div>
-          </WhiteBlock>
-        </div>
 
-        <div className="w-[450px]">
-          <CheckoutSidebar loading={loading} totalAmount={totalAmount} />
-        </div>
-      </div>
+            <div className="w-[450px]">
+              <CheckoutSidebar loading={loading} totalAmount={totalAmount} />
+            </div>
+          </div>
+        </form>
+      </FormProvider>
     </Container>
   );
 }
--- src/components/shared/form/form-input.tsx
+++ src/components/shared/form/form-input.tsx
@@ -15,7 +15,19 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const FormInput: FC<Props> = ({ className, name, label, required, ...props }) => {
-  const onClickClear = () => {};
+  const {
+    register,
+    formState: { errors },
+    watch,
+    setValue,
+  } = useFormContext();
+
+  const value = watch(name);
+  const errorText = errors[name]?.message as string;
+
+  const onClickClear = () => {
+    setValue(name, '', { shouldValidate: true });
+  };
 
   return (
     <div className={className}>
@@ -26,12 +38,12 @@ export const FormInput: FC<Props> = ({ className, name, label, required, ...prop
       )}
 
       <div className="relative">
-        <Input className="h-12 text-md" {...props} />
+        <Input className="h-12 text-md" {...register(name)} {...props} />
 
-        <ClearButton onClick={onClickClear} />
+        {value && <ClearButton onClick={onClickClear} />}
       </div>
 
-      <ErrorText text={'Error'} className="mt-2" />
+      {errorText && <ErrorText text={errorText} className="mt-2" />}
     </div>
   );
 };
