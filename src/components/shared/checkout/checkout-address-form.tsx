84a3071f7b1a971343f332160eaba6c582dd8112
Andrey Zuev <stepgt@mail.ru>
2025-08-06 15:55:59 +1000

    Added component AdressInput

--- src/components/shared/checkout/checkout-address-form.tsx
+++ src/components/shared/checkout/checkout-address-form.tsx
@@ -2,18 +2,30 @@
 
 import { FC } from 'react';
 import { WhiteBlock } from '../white-block';
-import { Input } from '@/components/ui';
 import { FormTextarea } from '../form';
+import { Controller, useFormContext } from 'react-hook-form';
+import { AdressInput } from '../address-input';
+import { ErrorText } from '../error-text';
 
 interface Props {
   className?: string;
 }
 
 export const CheckoutAddressForm: FC<Props> = ({ className }) => {
+  const { control } = useFormContext();
   return (
     <WhiteBlock title="3. Delivery address" className={className}>
       <div className="grid grid-col gap-5">
-        <Input name="address" className="text-base" placeholder="Address" />
+        <Controller
+          control={control}
+          name="address"
+          render={({ field, fieldState }) => (
+            <>
+              <AdressInput onChange={field.onChange} />
+              {fieldState.error?.message && <ErrorText text={fieldState.error.message} />}
+            </>
+          )}
+        />
         <FormTextarea
           name="comment"
           className="text-base"
