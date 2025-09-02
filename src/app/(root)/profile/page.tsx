c46e5f85fc404495f913ba06e326b60e98e1883b
Andrey Zuev <stepgt@mail.ru>
2025-09-02 15:37:04 +1000

    Added profile page && lib getUserSession

---
+++ src/app/(root)/profile/page.tsx
@@ -0,0 +1,19 @@
+import { getUserSession } from '@/lib/get-user-session';
+import { redirect } from 'next/navigation';
+import { prisma } from '../../../../prisma/prisma-client';
+
+export default async function ProfilePage() {
+  const session = await getUserSession();
+
+  if (!session) {
+    return redirect('/not-auth');
+  }
+
+  const user = await prisma.user.findFirst({ where: { id: Number(session?.id) } });
+
+  if (!user) {
+    return redirect('/not-auth');
+  }
+
+  return <div>Profile</div>;
+}
