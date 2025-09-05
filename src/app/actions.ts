cfdf7a68061cf15b41f231a877affb21c27df90c
Andrey Zuev <stepgt@mail.ru>
2025-09-05 15:17:11 +1000

    Added RegisterForm, VerificationUserTemplate components & Action registerUser

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,6 +8,7 @@ import { sendEmail } from '@/lib';
 import { PayOrderTemplate } from '@/components/shared/email-temapltes/pay-order';
 import { getUserSession } from '@/lib/get-user-session';
 import { hashSync } from 'bcrypt';
+import { VerificationUserTemplate } from '@/components/shared/email-temapltes/verification-user';
 
 export async function createOrder(data: CheckoutFormValues) {
   try {
@@ -115,6 +116,52 @@ export async function updateUserInfo(body: Prisma.UserUpdateInput) {
     });
   } catch (err) {
     console.log('Error [UPDATE_USER]', err);
+    throw err;
+  }
+}
+
+export async function registerUser(body: Prisma.UserCreateInput) {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (user) {
+      if (!user.verified) {
+        throw new Error('Почта не подтверждена');
+      }
+
+      throw new Error('Пользователь уже существует');
+    }
+
+    const createdUser = await prisma.user.create({
+      data: {
+        fullName: body.fullName,
+        email: body.email,
+        password: hashSync(body.password, 10),
+      },
+    });
+
+    const code = Math.floor(100000 + Math.random() * 900000).toString();
+
+    await prisma.verificationCode.create({
+      data: {
+        code,
+        userID: createdUser.id,
+      },
+    });
+
+    await sendEmail(
+      createdUser.email,
+      'Next Pizza / 📝 Подтверждение регистрации',
+      VerificationUserTemplate({
+        code,
+      }),
+    );
+  } catch (err) {
+    console.log('Error [CREATE_USER]', err);
     throw err;
   }
 }
--- src/components/shared/modals/forms/register-form.tsx
+++ src/components/shared/modals/forms/register-form.tsx
@@ -1,12 +1,62 @@
 'use client';
 
-import { FC } from 'react';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast from 'react-hot-toast';
+import { TFormRegisterValues, formRegisterSchema } from './schemas';
+import { FormInput } from '../../form';
+import { Button } from '@/components/ui';
+import { registerUser } from '@/app/actions';
 
 interface Props {
   onClose?: VoidFunction;
   onClickLogin?: VoidFunction;
 }
 
-export const RegisterForm: FC<Props> = ({ onClose, onClickLogin }) => {
-  return <>RegisterForm</>;
+export const RegisterForm: React.FC<Props> = ({ onClose, onClickLogin }) => {
+  const form = useForm<TFormRegisterValues>({
+    resolver: zodResolver(formRegisterSchema),
+    defaultValues: {
+      email: '',
+      fullName: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const onSubmit = async (data: TFormRegisterValues) => {
+    try {
+      await registerUser({
+        email: data.email,
+        fullName: data.fullName,
+        password: data.password,
+      });
+
+      toast.error('Регистрация успешна 📝. Подтвердите свою почту', {
+        icon: '✅',
+      });
+
+      onClose?.();
+    } catch (error) {
+      return toast.error('Неверный E-Mail или пароль', {
+        icon: '❌',
+      });
+    }
+  };
+
+  return (
+    <FormProvider {...form}>
+      <form className="flex flex-col gap-5" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormInput name="email" label="E-Mail" required />
+        <FormInput name="fullName" label="Полное имя" required />
+        <FormInput name="password" label="Пароль" type="password" required />
+        <FormInput name="confirmPassword" label="Подтвердите пароль" type="password" required />
+
+        <Button loading={form.formState.isSubmitting} className="h-12 text-base" type="submit">
+          Зарегистрироваться
+        </Button>
+      </form>
+    </FormProvider>
+  );
 };
